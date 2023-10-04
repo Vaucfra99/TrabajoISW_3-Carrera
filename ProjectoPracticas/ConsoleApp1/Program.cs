511c4c5cf4e75266a1f52ad87e55f48fe99422f4
capitanfimosis <victorau93@gmail.com>
2023-10-04 23:50:56 +0200

    Practica 1

---
+++ ProjectoPracticas/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static List<string> nationalities;
+        static void initList()
+        {
+            nationalities = new List<string>()
+            {
+                "Australian",
+                "Mongolian",
+                "Russian",
+                "Austrian",
+                "Brazilian"
+            };
+        }
+        static void Main(string[] args)
+        {
+            initList();
+            nationalities.Sort();
+            foreach (string nationality in nationalities)
+            {
+                Console.WriteLine(nationality);
+            }
+        }
+    }
+}
