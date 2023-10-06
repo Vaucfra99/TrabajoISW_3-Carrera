250bb743a6da16463d12654002fab1e6bca54fe8
capitanfimosis <victorau93@gmail.com>
2023-10-06 11:00:50 +0200

    Clase Member

--- ProjectoPracticas/ClassLibrary/BusinessLogic/Entities/Member.cs
+++ ProjectoPracticas/ClassLibrary/BusinessLogic/Entities/Member.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,18 @@ namespace UPVTube.BusinessLogic.Entities
 {
     public partial class Member
     {
+        public Member() { 
+            List<String> StudentDonains = new List<String>();
+
+            List<String> TeacherDomains = new List<String>();
+        }
+
+        public Member(String email, String fullName, DateTime lastAccessData, String nick, String password, List<String> StudentDomains, List<String> TeacherDomains, Member member, Content owner, Comment writer):this() { 
+        
+        }
+        
+
+        public Boolean isStudent() { }
+        public Boolean isTeacher() { }
     }
 }
--- ProjectoPracticas/ClassLibrary/Persistence/Entities/Member.cs
+++ ProjectoPracticas/ClassLibrary/Persistence/Entities/Member.cs
@@ -8,5 +8,15 @@ namespace UPVTube.Persistence.Entities
 {
     public partial class Member
     {
+        public String Email { get; set; }
+        public String FullName { get; set; }
+        public DateTime LastAccessData { get; set; }
+        public String Nick { get; set; }
+        public String Password { get; set; }
+        public List<String> StudentDomains { get; set; }
+        public List<String> TeacherDomains { get; set; }
+        public virtual Member member { get; set; }
+        public virtual Content Owner { get; set; }
+        public virtual Comment Writer { get; set; }
     }
 }
