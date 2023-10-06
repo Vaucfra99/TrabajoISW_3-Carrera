e0eddb235b554c95ced31a6e65aed68287217a77
Juan Candela Morant <JCANMOR@upv.edu.es>
2023-10-06 10:45:09 +0200

    Implementado subject y visualization en persistance

--- ProjectoPracticas/ClassLibrary/Persistence/Entities/Subject.cs
+++ ProjectoPracticas/ClassLibrary/Persistence/Entities/Subject.cs
@@ -8,5 +8,8 @@ namespace UPVTube.Persistence.Entities
 {
     public partial class Subject
     {
+        public int Code { get; set; }
+        public string FullName { get; set; }
+        public string Name { get; set; }
     }
 }
