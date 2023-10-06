b52a1c3d816d7aba99a84eca327b398add663142
Nacho de Lucas Gil <IDELUC@upv.edu.es>
2023-10-06 10:58:05 +0200

    Evaluation y comment

--- ProjectoPracticas/ClassLibrary/Persistence/Entities/Evaluation.cs
+++ ProjectoPracticas/ClassLibrary/Persistence/Entities/Evaluation.cs
@@ -8,5 +8,10 @@ namespace UPVTube.Persistence.Entities
 {
     public partial class Evaluation
     {
+        public int Id { get; set; }
+        public String RejectionReason { get; set; }
+        public DateTime EvaluationDate { get; set; }
+        public virtual Content Content { get; set; }
+        public virtual Member Censor { get; set; }
     }
 }
