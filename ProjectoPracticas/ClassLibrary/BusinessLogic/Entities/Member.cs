f4097da14cf2767b5f899098d239637cca0081bf
capitanfimosis <victorau93@gmail.com>
2023-10-08 03:45:53 +0200

    Cambios Member

--- ProjectoPracticas/ClassLibrary/BusinessLogic/Entities/Member.cs
+++ ProjectoPracticas/ClassLibrary/BusinessLogic/Entities/Member.cs
@@ -5,23 +5,39 @@ using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using UPVTube.Persistence.Entities;
 
 namespace UPVTube.BusinessLogic.Entities
 {
     public partial class Member
     {
-        public Member() { 
-            List<String> StudentDonains = new List<String>();
+        public Member() 
+        { 
+            List<String> StudentDomains = new List<String>();
 
             List<String> TeacherDomains = new List<String>();
         }
 
-        public Member(String email, String fullName, DateTime lastAccessData, String nick, String password, List<String> StudentDomains, List<String> TeacherDomains, Member member, Content owner, Comment writer):this() { 
+        public Member(String email, String fullName, DateTime lastAccessData, String nick, String password, 
+                      List<String> studentDomains, List<String> teacherDomains, Member member, Content owner, 
+                      Comment writer):this() 
+        {
+            this.Email = email;
+            this.Fullname = fullName;
+            this.LastAccessData = lastAccessData;
+            this.Nick = nick;
+            this.Password = password;
+            this.StudentDomains = studentDomains;
+            this.TeacherDomains = teacherDomains;
+            this.member = member;
+            this.Owner = owner;
+            this.Writer = writer;
         
         }
         
 
-        public Boolean isStudent() { }
-        public Boolean isTeacher() { }
+        private Boolean isStudent() { return false; }
+        private  Boolean isTeacher() { return false; }
+        }
     }
 }
