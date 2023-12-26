1592e604da6f7758a9a527747ba857861528c47f
capitanfimosis <victorau93@gmail.com>
2023-12-26 21:45:14 +0100

    Forms creado ahora es la buena, con Irene

---
+++ ProjectoPracticas/UPVTube.GUI/UPVTubeApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UPVTube.Services;
+
+namespace UPVTube.GUI
+{
+    public partial class UPVTubeApp : Form
+    {
+        private IUPVTubeService service;
+        public UPVTubeApp(IUPVTubeService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+    }
+}
