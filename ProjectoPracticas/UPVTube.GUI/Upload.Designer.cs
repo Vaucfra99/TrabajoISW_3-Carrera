b6e4aec704e1c481be36a79684d64832eca5b879
Nacho de Lucas Gil <IDELUC@upv.edu.es>
2024-01-08 14:24:27 +0100

    interfaz de upload

---
+++ ProjectoPracticas/UPVTube.GUI/Upload.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace UPVTube.GUI
+{
+    partial class Upload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDescripcion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxURI = new System.Windows.Forms.TextBox();
+            this.checkBoxPublico = new System.Windows.Forms.CheckBox();
+            this.buttonSubir = new System.Windows.Forms.Button();
+            this.buttonAtras = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subir contenido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Introduzca el título";
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(165, 84);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(210, 22);
+            this.textBoxTitulo.TabIndex = 2;
+            this.textBoxTitulo.TextChanged += new System.EventHandler(this.textBoxTitulo_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Introduzca la descripción";
+            // 
+            // textBoxDescripcion
+            // 
+            this.textBoxDescripcion.Location = new System.Drawing.Point(49, 148);
+            this.textBoxDescripcion.Multiline = true;
+            this.textBoxDescripcion.Name = "textBoxDescripcion";
+            this.textBoxDescripcion.Size = new System.Drawing.Size(326, 101);
+            this.textBoxDescripcion.TabIndex = 4;
+            this.textBoxDescripcion.TextChanged += new System.EventHandler(this.textBoxDescripcion_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Introduzca la URI";
+            // 
+            // textBoxURI
+            // 
+            this.textBoxURI.Location = new System.Drawing.Point(165, 269);
+            this.textBoxURI.Name = "textBoxURI";
+            this.textBoxURI.Size = new System.Drawing.Size(210, 22);
+            this.textBoxURI.TabIndex = 6;
+            this.textBoxURI.TextChanged += new System.EventHandler(this.textBoxURI_TextChanged);
+            // 
+            // checkBoxPublico
+            // 
+            this.checkBoxPublico.AutoSize = true;
+            this.checkBoxPublico.Location = new System.Drawing.Point(49, 310);
+            this.checkBoxPublico.Name = "checkBoxPublico";
+            this.checkBoxPublico.Size = new System.Drawing.Size(169, 20);
+            this.checkBoxPublico.TabIndex = 7;
+            this.checkBoxPublico.Text = "Desea que sea público";
+            this.checkBoxPublico.UseVisualStyleBackColor = true;
+            this.checkBoxPublico.CheckedChanged += new System.EventHandler(this.checkBoxPublico_CheckedChanged);
+            // 
+            // buttonSubir
+            // 
+            this.buttonSubir.Location = new System.Drawing.Point(165, 360);
+            this.buttonSubir.Name = "buttonSubir";
+            this.buttonSubir.Size = new System.Drawing.Size(96, 41);
+            this.buttonSubir.TabIndex = 8;
+            this.buttonSubir.Text = "SUBIR";
+            this.buttonSubir.UseVisualStyleBackColor = true;
+            this.buttonSubir.Click += new System.EventHandler(this.buttonSubir_Click);
+            // 
+            // buttonAtras
+            // 
+            this.buttonAtras.Location = new System.Drawing.Point(13, 415);
+            this.buttonAtras.Name = "buttonAtras";
+            this.buttonAtras.Size = new System.Drawing.Size(55, 23);
+            this.buttonAtras.TabIndex = 9;
+            this.buttonAtras.Text = "Atrás";
+            this.buttonAtras.UseVisualStyleBackColor = true;
+            this.buttonAtras.Click += new System.EventHandler(this.buttonAtras_Click);
+            // 
+            // Upload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 450);
+            this.Controls.Add(this.buttonAtras);
+            this.Controls.Add(this.buttonSubir);
+            this.Controls.Add(this.checkBoxPublico);
+            this.Controls.Add(this.textBoxURI);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxDescripcion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Upload";
+            this.Text = "Upload";
+            this.Load += new System.EventHandler(this.Upload_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDescripcion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxURI;
+        private System.Windows.Forms.CheckBox checkBoxPublico;
+        private System.Windows.Forms.Button buttonSubir;
+        private System.Windows.Forms.Button buttonAtras;
+    }
+}
---
+++ ProjectoPracticas/UPVTube.GUI/Upload.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UPVTube.Services;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace UPVTube.GUI
+{
+    public partial class Upload : Form
+    {
+        private IUPVTubeService service;
+        private String tittle;
+        private String description;
+        private String contentURI;
+        private Boolean isPublic = false;
+        
+        public Upload(IUPVTubeService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+
+        private void Upload_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBoxTitulo_TextChanged(object sender, EventArgs e)
+        {
+            tittle = textBoxTitulo.Text;
+
+        }
+
+        private void textBoxDescripcion_TextChanged(object sender, EventArgs e)
+        {
+            description = textBoxDescripcion.Text;
+        }
+
+        private void textBoxURI_TextChanged(object sender, EventArgs e)
+        {
+            contentURI = textBoxURI.Text;
+        }
+
+        private void checkBoxPublico_CheckedChanged(object sender, EventArgs e)
+        {
+            if (isPublic == true) { isPublic = false; }
+            else { isPublic = true; }
+        }
+
+        private void buttonSubir_Click(object sender, EventArgs e)
+        {
+            if (textBoxTitulo.Text == string.Empty || textBoxDescripcion.Text == string.Empty || textBoxURI.Text == string.Empty)
+            {
+                DialogResult error = MessageBox.Show(this, "Complete todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBoxTitulo.Clear();
+                textBoxDescripcion.Clear();
+                textBoxURI.Clear();
+            }
+            else
+            {
+            
+                try
+                {
+                    service.Upload(tittle, description, contentURI, isPublic);
+                }
+                catch (ServiceException ex)
+                {
+                    DialogResult answer = MessageBox.Show(this, // Owner
+                    ex.Message, // Message
+                    "Error de Servicio", // Title
+                    MessageBoxButtons.OK, // Buttons included
+                    MessageBoxIcon.Exclamation); // Icon
+                }
+
+            }
+        }
+
+        private void buttonAtras_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
