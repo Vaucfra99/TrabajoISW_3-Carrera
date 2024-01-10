dab23543ea7ad7c5f3f1b5654e9b831feb7af317
Irene3 <irenesanromanf3@gmail.com>
2024-01-10 02:07:53 +0100

    jhbjhb

--- ProjectoPracticas/UPVTube.GUI/Menu.cs
+++ ProjectoPracticas/UPVTube.GUI/Menu.cs
@@ -46,6 +46,7 @@ namespace UPVTube.GUI
             this.Hide();
             search.ShowDialog();
             this.Close();
+
         }
 
         private void label1_Click(object sender, EventArgs e)
