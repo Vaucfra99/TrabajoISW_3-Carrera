bc0d67de01f58d14fd120521b8e2ff3cb5cbb963
capitanfimosis <victorau93@gmail.com>
2024-01-11 12:05:13 +0100

    Login y UPVTubeApp completamente terminados

--- ProjectoPracticas/UPVTube.GUI/Login.Designer.cs
+++ ProjectoPracticas/UPVTube.GUI/Login.Designer.cs
@@ -35,65 +35,62 @@
             this.TextBoxPassword = new System.Windows.Forms.TextBox();
             this.BtnAceptar = new System.Windows.Forms.Button();
             this.BtnClear = new System.Windows.Forms.Button();
-            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnReturn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 24);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(80, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.Size = new System.Drawing.Size(102, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Inicio de Sesion";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 75);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(19, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(57, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Usuario:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 135);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(17, 166);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.Size = new System.Drawing.Size(79, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Contraseña:";
             // 
             // TextBoxUser
             // 
             this.TextBoxUser.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.TextBoxUser.Location = new System.Drawing.Point(88, 72);
-            this.TextBoxUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBoxUser.Location = new System.Drawing.Point(117, 89);
+            this.TextBoxUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextBoxUser.Name = "TextBoxUser";
-            this.TextBoxUser.Size = new System.Drawing.Size(75, 20);
+            this.TextBoxUser.Size = new System.Drawing.Size(139, 22);
             this.TextBoxUser.TabIndex = 3;
             this.TextBoxUser.TextChanged += new System.EventHandler(this.TextBoxUser_TextChanged);
             // 
             // TextBoxPassword
             // 
-            this.TextBoxPassword.Location = new System.Drawing.Point(88, 130);
-            this.TextBoxPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBoxPassword.Location = new System.Drawing.Point(117, 160);
+            this.TextBoxPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TextBoxPassword.Name = "TextBoxPassword";
             this.TextBoxPassword.PasswordChar = '*';
-            this.TextBoxPassword.Size = new System.Drawing.Size(77, 20);
+            this.TextBoxPassword.Size = new System.Drawing.Size(139, 22);
             this.TextBoxPassword.TabIndex = 4;
             this.TextBoxPassword.TextChanged += new System.EventHandler(this.TextBoxPassword_TextChanged);
             // 
             // BtnAceptar
             // 
-            this.BtnAceptar.Location = new System.Drawing.Point(122, 165);
-            this.BtnAceptar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnAceptar.Location = new System.Drawing.Point(182, 213);
+            this.BtnAceptar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnAceptar.Name = "BtnAceptar";
-            this.BtnAceptar.Size = new System.Drawing.Size(66, 32);
+            this.BtnAceptar.Size = new System.Drawing.Size(74, 39);
             this.BtnAceptar.TabIndex = 5;
             this.BtnAceptar.Text = "Aceptar";
             this.BtnAceptar.UseVisualStyleBackColor = true;
@@ -101,31 +98,32 @@
             // 
             // BtnClear
             // 
-            this.BtnClear.Location = new System.Drawing.Point(23, 165);
-            this.BtnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnClear.Location = new System.Drawing.Point(20, 213);
+            this.BtnClear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnClear.Name = "BtnClear";
-            this.BtnClear.Size = new System.Drawing.Size(54, 32);
+            this.BtnClear.Size = new System.Drawing.Size(76, 39);
             this.BtnClear.TabIndex = 6;
             this.BtnClear.Text = "Borrar";
             this.BtnClear.UseVisualStyleBackColor = true;
             this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
             // 
-            // BtnExit
+            // BtnReturn
             // 
-            this.BtnExit.Location = new System.Drawing.Point(138, 202);
-            this.BtnExit.Name = "BtnExit";
-            this.BtnExit.Size = new System.Drawing.Size(60, 42);
-            this.BtnExit.TabIndex = 7;
-            this.BtnExit.Text = "Salir";
-            this.BtnExit.UseVisualStyleBackColor = true;
-            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            this.BtnReturn.Location = new System.Drawing.Point(22, 302);
+            this.BtnReturn.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnReturn.Name = "BtnReturn";
+            this.BtnReturn.Size = new System.Drawing.Size(76, 39);
+            this.BtnReturn.TabIndex = 7;
+            this.BtnReturn.Text = "Volver";
+            this.BtnReturn.UseVisualStyleBackColor = true;
+            this.BtnReturn.Click += new System.EventHandler(this.BtnReturn_Click);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(210, 250);
-            this.Controls.Add(this.BtnExit);
+            this.ClientSize = new System.Drawing.Size(280, 354);
+            this.Controls.Add(this.BtnReturn);
             this.Controls.Add(this.BtnClear);
             this.Controls.Add(this.BtnAceptar);
             this.Controls.Add(this.TextBoxPassword);
@@ -133,7 +131,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
@@ -151,6 +149,6 @@
         private System.Windows.Forms.TextBox TextBoxPassword;
         private System.Windows.Forms.Button BtnAceptar;
         private System.Windows.Forms.Button BtnClear;
-        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnReturn;
     }
 }
