13e43aab5b99756b9d2eef7c443d6668a2533792
Nacho de Lucas Gil <IDELUC@upv.edu.es>
2024-01-11 13:29:53 +0100

    Pestaña Evaluar hecho

--- ProjectoPracticas/UPVTube.GUI/Evaluar.Designer.cs
+++ ProjectoPracticas/UPVTube.GUI/Evaluar.Designer.cs
@@ -28,20 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listaPendientes = new System.Windows.Forms.ListView();
+            this.textBoxComentario = new System.Windows.Forms.TextBox();
+            this.buttonAutorizar = new System.Windows.Forms.Button();
+            this.buttonDenegar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listaPendientes
+            // 
+            this.listaPendientes.HideSelection = false;
+            this.listaPendientes.Location = new System.Drawing.Point(22, 57);
+            this.listaPendientes.Name = "listaPendientes";
+            this.listaPendientes.Size = new System.Drawing.Size(751, 117);
+            this.listaPendientes.TabIndex = 0;
+            this.listaPendientes.UseCompatibleStateImageBehavior = false;
+            this.listaPendientes.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listaPendientes_SelectedIndexChanged);
+            // 
+            // textBoxComentario
+            // 
+            this.textBoxComentario.Location = new System.Drawing.Point(22, 243);
+            this.textBoxComentario.Multiline = true;
+            this.textBoxComentario.Name = "textBoxComentario";
+            this.textBoxComentario.Size = new System.Drawing.Size(751, 102);
+            this.textBoxComentario.TabIndex = 1;
+            // 
+            // buttonAutorizar
+            // 
+            this.buttonAutorizar.Location = new System.Drawing.Point(587, 391);
+            this.buttonAutorizar.Name = "buttonAutorizar";
+            this.buttonAutorizar.Size = new System.Drawing.Size(84, 37);
+            this.buttonAutorizar.TabIndex = 2;
+            this.buttonAutorizar.Text = "Autorizar";
+            this.buttonAutorizar.UseVisualStyleBackColor = true;
+            this.buttonAutorizar.Click += new System.EventHandler(this.buttonAutorizar_Click);
+            // 
+            // buttonDenegar
+            // 
+            this.buttonDenegar.Location = new System.Drawing.Point(688, 391);
+            this.buttonDenegar.Name = "buttonDenegar";
+            this.buttonDenegar.Size = new System.Drawing.Size(85, 37);
+            this.buttonDenegar.TabIndex = 3;
+            this.buttonDenegar.Text = "Denegar";
+            this.buttonDenegar.UseVisualStyleBackColor = true;
+            this.buttonDenegar.Click += new System.EventHandler(this.buttonDenegar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Evaluar Contenido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Motivo del rechazo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Atras";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Evaluar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDenegar);
+            this.Controls.Add(this.buttonAutorizar);
+            this.Controls.Add(this.textBoxComentario);
+            this.Controls.Add(this.listaPendientes);
             this.Name = "Evaluar";
             this.Text = "Evaluar";
-            this.Load += new System.EventHandler(this.Evaluar_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listaPendientes;
+        private System.Windows.Forms.TextBox textBoxComentario;
+        private System.Windows.Forms.Button buttonAutorizar;
+        private System.Windows.Forms.Button buttonDenegar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- ProjectoPracticas/UPVTube.GUI/Evaluar.cs
+++ ProjectoPracticas/UPVTube.GUI/Evaluar.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UPVTube.Entities;
 using UPVTube.Services;
 
 namespace UPVTube.GUI
@@ -14,13 +15,107 @@ namespace UPVTube.GUI
     public partial class Evaluar : Form
     {
         private IUPVTubeService service;
+        private bool evaluacion;
         public Evaluar(IUPVTubeService service)
         {
             InitializeComponent();
             this.service = service;
+            ConfigurarListView();
+            CargarDatosEnListView();
+            Desactiva_Botones();
         }
 
-        private void Evaluar_Load(object sender, EventArgs e)
+        private void listaPendientes_SelectedIndexChanged(object sender, MouseEventArgs e)
+        {
+            int filas_seleccionadas = listaPendientes.SelectedItems.Count;
+            if (filas_seleccionadas > 0)
+            {
+                Activa_Botones();
+            }
+            else 
+            {
+                Desactiva_Botones();
+            }
+        }
+
+        private void ConfigurarListView()
+        {
+            listaPendientes.View = View.Details;
+            listaPendientes.Columns.Add("ID", -2);
+            listaPendientes.Columns.Add("Título", -2);
+            listaPendientes.Columns.Add("Creador", -2);
+            listaPendientes.Columns.Add("Descripción", 300);
+            listaPendientes.Columns.Add("URI", 300);
+            listaPendientes.Columns.Add("Asignaturas", -2);
+            listaPendientes.Columns.Add("Fecha de Subida", -2);
+        }
+
+        private void CargarDatosEnListView()
+        {
+            List<Content> contents = service.GetPendingContents();
+
+
+            foreach (Content content in contents)
+            {
+                ListViewItem item = new ListViewItem(content.Id.ToString());
+                item.SubItems.Add(content.Title);
+                item.SubItems.Add(content.Owner.Nick);
+                item.SubItems.Add(content.Description);
+                item.SubItems.Add(content.ContentURI);
+                item.SubItems.Add(string.Join(", ", content.Subjects.Select(s => s.Name)));
+                item.SubItems.Add(content.UploadDate.ToString("dd-MM-yyyy"));
+                item.SubItems.Add(content.Visualizations.Count.ToString());
+                item.SubItems.Add(content.Comments.Count.ToString());
+
+                listaPendientes.Items.Add(item);
+            }
+        }
+
+  
+
+        private void buttonAutorizar_Click(object sender, EventArgs e)
+        {
+            evaluacion = true;
+            Desactiva_Botones();
+            int id;
+            int.TryParse(listaPendientes.SelectedItems[0].Text, out id);
+            service.EvaluarContent(id, evaluacion, textBoxComentario.Text);
+            listaPendientes.Items.Clear();
+            CargarDatosEnListView();
+        }
+
+        private void buttonDenegar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxComentario.Text))
+            {
+                MessageBox.Show("No puedes dejar vacio el comentario");
+                return;
+            }
+            evaluacion = false;
+            Desactiva_Botones();
+            int id;
+            int.TryParse(listaPendientes.SelectedItems[0].Text, out id);
+            service.EvaluarContent(id, evaluacion, textBoxComentario.Text);
+            listaPendientes.Items.Clear();
+            CargarDatosEnListView();
+            textBoxComentario.Text = string.Empty;
+        }
+
+        private void Desactiva_Botones()
+        {
+            buttonAutorizar.Enabled = false;
+            buttonDenegar.Enabled = false;
+            textBoxComentario.Enabled = false;
+        }
+
+        private void Activa_Botones()
+        {
+            buttonAutorizar.Enabled = true;
+            buttonDenegar.Enabled = true;
+            textBoxComentario.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
