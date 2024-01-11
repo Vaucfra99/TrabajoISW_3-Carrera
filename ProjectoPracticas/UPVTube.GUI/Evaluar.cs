3330a595656c7b019acf2eb15c7cc0d41b95b995
Irene3 <irenesanromanf3@gmail.com>
2024-01-12 00:03:28 +0100

    maaaaaaaaas correcciones

--- ProjectoPracticas/UPVTube.GUI/Evaluar.cs
+++ ProjectoPracticas/UPVTube.GUI/Evaluar.cs
@@ -18,28 +18,17 @@ namespace UPVTube.GUI
         private IUPVTubeService service;
         private bool evaluacion;
         private Menu meenu;
+        private Evaluacion evaluacion;
         public Evaluar(IUPVTubeService service)
         {
             InitializeComponent();
             this.service = service;
             meenu = new Menu(service);
+            evaluacion = new Evaluar.evaluacion(service);
             ConfigurarListView();
             CargarDatosEnListView();
-            Desactiva_Botones();
         }
 
-        private void listaPendientes_SelectedIndexChanged(object sender, MouseEventArgs e)
-        {
-            int filas_seleccionadas = listaPendientes.SelectedItems.Count;
-            if (filas_seleccionadas > 0)
-            {
-                Activa_Botones();
-            }
-            else 
-            {
-                Desactiva_Botones();
-            }
-        }
 
         private void ConfigurarListView()
         {
@@ -55,54 +44,25 @@ namespace UPVTube.GUI
 
         private void CargarDatosEnListView()
         {
-            List<Content> contents = service.GetPendingContents();
-
-
-
-            foreach (Content content in contents)
+            try
             {
-                ListViewItem item = new ListViewItem(content.Id.ToString());
-                item.SubItems.Add(content.Title);
-                item.SubItems.Add(content.Owner.Nick);
-                item.SubItems.Add(content.Description);
-                item.SubItems.Add(content.ContentURI);
-                item.SubItems.Add(string.Join(", ", content.Subjects.Select(s => s.Name)));
-                item.SubItems.Add(content.UploadDate.ToString("dd-MM-yyyy"));
-                item.SubItems.Add(content.Visualizations.Count.ToString());
-                item.SubItems.Add(content.Comments.Count.ToString());
+                List<Content> cList = service.GetPendingContents();
+                foreach (Content c in cList)
+                {
 
-                listaPendientes.Items.Add(item);
+
+                    listaPendientes.Items.Add(c);
+                }
+
             }
-        }
-
-  
-
-        private void buttonAutorizar_Click(object sender, EventArgs e)
-        {
-            evaluacion = true;
-            Desactiva_Botones();
-            int id;
-            int.TryParse(listaPendientes.SelectedItems[0].Text, out id);
-            service.EvaluarContent(id, evaluacion, textBoxComentario.Text);
-            listaPendientes.Items.Clear();
-            CargarDatosEnListView();
-        }
-
-        private void buttonDenegar_Click(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(textBoxComentario.Text))
+            catch (ServiceException ex)
             {
-                MessageBox.Show("No puedes dejar vacio el comentario");
-                return;
+                DialogResult answer = MessageBox.Show(this, // Owner
+                ex.Message, // Message
+                "Error de Servicio", // Title
+                MessageBoxButtons.OK, // Buttons included
+                MessageBoxIcon.Exclamation); // Icon
             }
-            evaluacion = false;
-            Desactiva_Botones();
-            int id;
-            int.TryParse(listaPendientes.SelectedItems[0].Text, out id);
-            service.EvaluarContent(id, evaluacion, textBoxComentario.Text);
-            listaPendientes.Items.Clear();
-            CargarDatosEnListView();
-            textBoxComentario.Text = string.Empty;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -114,10 +74,13 @@ namespace UPVTube.GUI
 
         private void listaPendientes_SelectedIndexChanged(object sender, EventArgs e)
         {
+            Content c1 = (Content)listaPendientes.SelectedItem;
 
-
-
+            this.Hide();
+            evaluacion.ShowDialog();
+            this.Close();
         }
-
     }
+
 }
+
